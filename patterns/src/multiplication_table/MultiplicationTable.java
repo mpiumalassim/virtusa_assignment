ce7d4811928cf889f8eb23e019553bb14b9da319
mpiumalassim <59538053+mpiumalassim@users.noreply.github.com>
2020-01-13 19:44:33 +0530

    Multiplication table and Diamond star pattern

---
+++ patterns/src/multiplication_table/MultiplicationTable.java
@@ -0,0 +1,25 @@
+package multiplication_table;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int tableSize = 9;
+        printMultiplicationTable(tableSize);
+    }
+
+    public static void printMultiplicationTable(int tableSize) {
+        System.out.format("      ");
+        for(int i = 1; i<=tableSize;i++ ) {
+            System.out.format("%4d",i);
+        }
+        System.out.println();
+        System.out.println("------------------------------------------");
+
+        for(int i = 1 ;i<=tableSize;i++) {
+            System.out.format("%4d |",i);
+            for(int j=1;j<=tableSize;j++) {
+                System.out.format("%4d",i*j);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ patterns/src/starpatters/Diamond.java
@@ -0,0 +1,38 @@
+package starpatters;
+
+import java.util.Scanner;
+
+public class Diamond {
+    public static void main(String args[])
+    {
+        int n = 8, i, j, space = 1;
+        space = n - 1;
+        for (j = 1; j <= n; j++)
+        {
+            for (i = 1; i <= space; i++)
+            {
+                System.out.print(" ");
+            }
+            space--;
+            for (i = 1; i <= 2 * j - 1; i++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        space = 1;
+        for (j = 1; j <= n - 1; j++)
+        {
+            for (i = 1; i <= space; i++)
+            {
+                System.out.print(" ");
+            }
+            space++;
+            for (i = 1; i <= 2 * (n - j) - 1; i++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
